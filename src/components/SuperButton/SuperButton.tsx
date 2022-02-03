5d825f8276fcbc0879d1f5103891741accb0b9dc
vladward <vladward13@gmail.com>
2022-02-03 21:19:16 +0300

    feature: add super button

---
+++ src/components/SuperButton/SuperButton.tsx
@@ -0,0 +1,26 @@
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from './SuperButton.module.css'
+
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+type SuperButtonPropsType = DefaultButtonPropsType & {
+    red?: boolean
+}
+
+const SuperButton: React.FC<SuperButtonPropsType> = (
+    {
+        red, className,
+        ...restProps
+    }
+) => {
+    const finalClassName = `${s.button} ${red ? s.red : s.default} ${className}`
+
+    return (
+        <button
+            className={finalClassName}
+            {...restProps}
+        />
+    )
+}
+
+export default SuperButton
