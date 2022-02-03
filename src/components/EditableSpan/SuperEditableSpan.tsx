1d2e4de715ccd027fa5259da29b2602165937a6b
vladward <vladward13@gmail.com>
2022-02-03 21:19:03 +0300

    feature: add editable span

---
+++ src/components/EditableSpan/SuperEditableSpan.tsx
@@ -0,0 +1,76 @@
+import React, {DetailedHTMLProps, InputHTMLAttributes, HTMLAttributes, useState} from 'react'
+import s from './styles.module.css'
+import SuperInputText from "../SuperInput/SuperInputText";
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultSpanPropsType = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+
+type SuperEditableSpanType = DefaultInputPropsType & {
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: string
+    spanClassName?: string
+
+    spanProps?: DefaultSpanPropsType
+}
+
+const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
+    {
+        autoFocus,
+        onBlur,
+        onEnter,
+        spanProps,
+
+        ...restProps
+    }
+) => {
+    const [editMode, setEditMode] = useState<boolean>(false)
+    const {children, onDoubleClick, className, ...restSpanProps} = spanProps || {}
+
+    const onEnterCallback = () => {
+        setEditMode(false)
+
+        onEnter && onEnter()
+    }
+    const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
+        setEditMode(false)
+
+        onBlur && onBlur(e)
+    }
+    const onDoubleClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+        setEditMode(true)
+
+        onDoubleClick && onDoubleClick(e)
+    }
+
+    const spanClassName = `${s.span} ${className} ? className : ''`
+
+    return (
+        <>
+            {editMode
+                ? (
+                    <SuperInputText
+                        autoFocus
+                        onBlur={onBlurCallback}
+                        onEnter={onEnterCallback}
+                        style={{border: "1px solid rosybrown", color: "rosybrown"}}
+
+                        {...restProps}
+                    />
+                ) : (
+                    <span
+                        onDoubleClick={onDoubleClickCallBack}
+                        className={spanClassName}
+                        style={{color: "rosybrown"}}
+
+                        {...restSpanProps}
+                    >
+                        ✎{children || restProps.value}
+                    </span>
+                )
+            }
+        </>
+    )
+}
+
+export default SuperEditableSpan
