908231c4df489aaafd26ec3da2bc7e3dcdef402e
vladward <vladward13@gmail.com>
2022-02-03 21:19:43 +0300

    feature: add super input

---
+++ src/components/SuperInput/SuperInputText.tsx
@@ -0,0 +1,56 @@
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent} from 'react'
+import s from './SuperInputText.module.css'
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type SuperInputTextPropsType = DefaultInputPropsType & {
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: string
+    spanClassName?: string
+}
+
+const SuperInputText: React.FC<SuperInputTextPropsType> = (
+    {
+        type,
+        onChange, onChangeText,
+        onKeyPress, onEnter,
+        error,
+        className, spanClassName,
+
+        ...restProps
+    }
+) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange
+        && onChange(e)
+
+        onChangeText && onChangeText(e.currentTarget.value)
+    }
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+        onKeyPress && onKeyPress(e);
+
+        onEnter
+        && e.key === 'Enter'
+        && onEnter()
+    }
+
+    const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
+    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}` // need to fix with (?:) and s.superInput
+
+    return (
+        <>
+            <input
+                type={'text'}
+                onChange={onChangeCallback}
+                onKeyPress={onKeyPressCallback}
+                className={finalInputClassName}
+
+                {...restProps}
+            />
+            {error && <span className={finalSpanClassName}>{error}</span>}
+        </>
+    )
+}
+
+export default SuperInputText
